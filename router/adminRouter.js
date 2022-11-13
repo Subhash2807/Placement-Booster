7242113c04671f0602d813badbf5f849a354271e
subhash2807 <lcs2019016@iiitl.ac.in>
2022-11-13 14:53:53 +0530

    Mcq added

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,12 +1,32 @@
 const { checkAuthenticated, isAdmin } = require('./middlewares/auth');
+const Question = require('./../db/models/questions')
 const router = require('express').Router();
 
-router.get('/',isAdmin,(req,res)=>{
+router.get('/',(req,res)=>{
     res.render("admin");
 })
 
 router.get('/add-question',(req,res)=>{
     res.render('add-question');
 })
+router.post('/add-question',async (req,res)=>{
+    const {question,op1,op2,op3,op4,correct,subject} = req.body;
+    if(!question || !op1 || !op2 || !op3 || !op4 || !correct){
+        res.send("fill all fields");
+    }
+    else
+    {
+    const question = new Question(req.body);
+        try{
+            await question.save();
+            console.log(question);
+            res.render('add-question');
+        }
+        catch(e){
+            res.send(e);
+        }
+    }
+})
+
 
 module.exports = router;
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
+const Question = require('./../db/models/questions')
 const {checkAuthenticated,loginCheck} = require('./middlewares/auth')
 
 router.get('/',checkAuthenticated,(req,res)=>{
     console.log(req.user);
     res.render('home',{user:req.user});
 })
+router.get('/take-test',(req,res)=>{
+    res.render("test");
+})
+router.get('/get-question',(req,res)=>{
+    Question.count().exec(function (err, count) {
 
+        // Get a random entry
+        var random = Math.floor(Math.random() * count)
+      
+        // Again query all users but only fetch one offset by our random #
+        Question.findOne().skip(random).exec(
+          function (err, result) {
+            // Tada! random user
+            console.log(result) 
+            res.send(result)
+          })
+      })
+})
 module.exports = router;
